92f603eb8bf131b0633215ebf0577699a42c0524
Dmitry Shkoliar <7448756+shkoliar@users.noreply.github.com>
2020-01-14 22:17:38 +0200

    Added modules registration.php and module.xml

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * GridColors
+ *
+ * @copyright Copyright © 2020 Dmitry Shkoliar. All rights reserved.
+ * @author    Dmitry Shkoliar
+ * @website   shkoliar.com
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Shkoliar_GridColors',
+    __DIR__
+);
